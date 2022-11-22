297568426e6aae0b65a2d2cf21d48996e295dd50
Szymon Datko <szymon.datko@pwr.edu.pl>
2022-11-22 13:36:30 +0100

    Update README.md
    This commit introduces the example of usage in Python script,
    as well as provides necessary details of installation and testing
    withing the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # PyOpenSet
-A Python library for performing outlier detection / implementing open-set classification in high-dimensional data.
+
+A Python library for performing outlier detection
+/ implementing open-set classification in high-dimensional data.
+
+
+## Install
+
+The project is still in development.
+Until there is official PyPI release,
+one may install the module the following way:
+
+```
+pip install git+https://github.com/sdatko/PyOpenSet.git@master
+```
+
+For development purposes it may be easier to clone the repository
+and install the editable version of this module:
+
+```
+pip install -e /path/to/the/clonned/repository
+```
+
+
+## Usage
+
+See the files in `examples/` directory for reference.
+
+
+## Tests
+
+Call `tox` to run the default test suite in this repository.
---
+++ examples/distances.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from openset.data.simulated import cluster
+from openset.models import Correlation
+from openset.models import Cosine
+from openset.models import LocalOutlierFactor
+from openset.models import Mahalanobis
+from openset.models import Manhattan
+from openset.models import Minkowski
+from openset.models import SEuclidean
+
+
+samples = 10
+dimension = 3
+
+
+def main():
+    training = cluster(samples=samples, dimension=dimension, location=0)
+    typicals = cluster(samples=5, dimension=dimension, location=0)
+    outliers = cluster(samples=2, dimension=dimension, location=2)
+
+    models = [
+        Correlation,
+        Cosine,
+        LocalOutlierFactor,
+        Mahalanobis,
+        Manhattan,
+        Minkowski,
+        SEuclidean,
+    ]
+
+    for model_class in models:
+        model = model_class()
+        model.fit(training)
+        known = model.score(typicals)
+        unknown = model.score(outliers)
+
+        print(model, known, unknown)
+
+
+if __name__ == '__main__':
+    main()
