3285d33999e1aea51891d03c6c2481aa1a013a02
Szymon Datko <szymon.datko@pwr.edu.pl>
2022-12-09 14:39:34 +0100

    Introduce kNearestNeighbor-based distance model

--- openset/models/__init__.py
+++ openset/models/__init__.py
@@ -2,6 +2,7 @@
 
 from ..models.correlation import Correlation
 from ..models.cosine import Cosine
+from ..models.knn import KNearestNeighbors
 from ..models.lof import LocalOutlierFactor
 from ..models.mahalanobis import Mahalanobis
 from ..models.manhattan import Manhattan
@@ -12,6 +13,7 @@ from ..models.seuclidean import SEuclidean
 __all__ = [
     'Correlation',
     'Cosine',
+    'KNearestNeighbors',
     'LocalOutlierFactor',
     'Mahalanobis',
     'Manhattan',
