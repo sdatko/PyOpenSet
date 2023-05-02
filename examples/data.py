a46cd1e1109fff7085521d354fee3d63a71338ca
Szymon Datko <szymon.datko@pwr.edu.pl>
2023-05-02 02:52:36 +0200

    Add MVN generator code
    This commit introduces additional function to data generator
    that allows producing data cluster with specified correlation
    and shift only on part of features.

--- examples/data.py
+++ examples/data.py
@@ -14,10 +14,14 @@ def main():
     data1 = generator.gaussian(samples, dimension)
     data2 = generator.triangular(samples, dimension)
     data3 = generator.uniform(samples, dimension)
+    data4 = generator.mvn(samples, dimension,
+                          location=5.0, scale=1.0,
+                          n_features=0.75, n_correlated=0.5, covariance=0.25)
 
     print(data1)
     print(data2)
     print(data3)
+    print(data4)
 
 
 if __name__ == '__main__':
