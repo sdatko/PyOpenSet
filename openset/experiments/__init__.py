6fdd07bf0072b38edf01e03c356365c88179eda8
Szymon Datko <szymon.datko@pwr.edu.pl>
2023-05-16 23:49:49 +0200

    Add properties experiment code

--- openset/experiments/__init__.py
+++ openset/experiments/__init__.py
@@ -3,10 +3,12 @@
 from openset.experiments.base import BaseExperiment
 from openset.experiments.distributions import Generated
 from openset.experiments.overlapping import BoundingBoxes
+from openset.experiments.properties import MVNEstimation
 
 
 __all__ = [
     'BaseExperiment',
     'BoundingBoxes',
     'Generated',
+    'MVNEstimation',
 ]
