a8fef664a739825b859de2227265ffa16d428efe
Szymon Datko <szymon.datko@pwr.edu.pl>
2023-05-16 23:21:34 +0200

    Add overlapping experiment code

--- openset/experiments/__init__.py
+++ openset/experiments/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+
 from openset.experiments.distributions import Generated
+from openset.experiments.overlapping import BoundingBoxes
 
 
 __all__ = [
+    'BoundingBoxes',
     'Generated',
 ]
